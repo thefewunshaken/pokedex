4132587d85d8561e50bf078d2fe4cd0cc549c021
Jeremy Kimble <kimblejeremy18@gmail.com>
2019-03-11 19:07:26 -0500

    Updates

---
+++ precache-manifest.5d174fe464e6a2bee08661bd0787b49b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "a8ad72513988fe430ead",
+    "url": "/pokedex/static/js/main.5cc67eda.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/pokedex/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "a8ad72513988fe430ead",
+    "url": "/pokedex/static/css/main.2b6053ee.chunk.css"
+  },
+  {
+    "revision": "c0a9bb794b042c9594dbba479037b36b",
+    "url": "/pokedex/index.html"
+  }
+];
